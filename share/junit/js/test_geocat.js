171273b4a63285bc87b4ca1cce2089f788093a9a
bolollo <bolosig@gmail.com>
2013-07-17 09:03:10 +0200

    unit test gestion usuarios

--- share/junit/js/test_geocat.js
+++ share/junit/js/test_geocat.js
@@ -1,7 +1,15 @@
 var urls = {
-	updatePassword: "http://localhost:8080/geocat/admin/updatePassword.action?",
+	login: "http://localhost:8080/geocat/login.action?",
+	createUser: "http://localhost:8080/geocat/admin/createUser.action?",
+	getUser: "http://localhost:8080/geocat/admin/getUser.action?",
+	updatePassword: "http://localhost:8080/geocat/user/updatePassword.action?",
+	updateUser: "http://localhost:8080/geocat/user/updateUser.action?",
 	llistarUsuaris: "http://localhost:8080/geocat/admin/llistarUsuaris.action?",
 	llistarUsuarisJson: "http://localhost:8080/geocat/admin/llistarUsuarisJson.action?",
+	getAllUsers: "http://localhost:8080/geocat/admin/getAllUsers.action?",
+	getAllUserNames: "http://localhost:8080/geocat/admin/getAllUserNames.action?",
+	deleteUser: "http://localhost:8080/geocat/admin/deleteUser.action?",
+	logout: "http://localhost:8080/geocat/logout.action?",
 };
 
 var JSV = require("./jsv").JSV;
@@ -12,6 +20,207 @@ test( "hello test", function() {
 });
 
 module( "admin" );
+asyncTest( "login", 1, function() {
+	$.ajax({
+		url: urls.login,
+		data: {user: 'wszczerban', password:'piji23'},
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {OK: "Usuari login"}, "Passed:"+ JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
+asyncTest( "createUser", 1, function() {
+	$.ajax({
+		url: urls.createUser,
+		data: {cn: 'Victor', sn:'Pascual', uid: 'vpascual', email: 'victor.pascual@icc.cat', userPassword: 'idec2009'},
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {OK: "User create"}, "Passed:"+ JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
+asyncTest( "updatePassword", 1, function() {
+	$.ajax({
+		url: urls.updatePassword,
+		data: {uid: 'vpascual', userPassword: 'idec2009', newPassword: 'idec2001'},
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {OK: "Password change"}, "Passed:"+ JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
+asyncTest( "updatePassword no new password", 1, function() {
+	$.ajax({
+		url:  urls.updatePassword,
+		data: {uid: 'vpascual', userPassword: 'idec2009'},
+		method: 'post',
+		dataType: 'jsonp',
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {ERROR: "Arguments no vàlids"}, "Passed:" + JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail!:" + results );
+		start();
+	});
+});
+
+asyncTest( "updatePassword no password", 1, function() {
+	$.ajax({
+		url:  urls.updatePassword,
+		data: {uid: 'vpascual', newPassword: 'idec2001'},
+		method: 'post',
+		dataType: 'jsonp',
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {ERROR: "Arguments no vàlids"}, "Passed:" + JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail!:" + results );
+		start();
+	});
+});
+
+asyncTest( "updatePassword old password wrong", 1, function() {
+	$.ajax({
+		url:  urls.updatePassword,
+		data: {uid: 'vpascual', userPassword: 'idec2009', newPassword: 'idec2001'},
+		method: 'post',
+		dataType: 'jsonp',
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {ERROR: "Wrong old password"}, "Passed:" + JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail!:" + results );
+		start();
+	});
+});
+
+asyncTest( "updatePassword Password present in password history", 1, function() {
+	$.ajax({
+		url:  urls.updatePassword,
+		data: {uid: 'vpascual', userPassword: 'idec2001', newPassword: 'idec2009'},
+		method: 'post',
+		dataType: 'jsonp',
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {ERROR: "Password present in password history"}, "Passed:" + JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail!:" + results );
+		start();
+	});
+});
+
+asyncTest( "updateUser", 1, function() {
+	$.ajax({
+		url: urls.updateUser,
+		data: {cn: 'Victor', sn:'Pascual Ayats', uid: 'vpascual', email: 'victor.pascual@icc.cat'},
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		var schema = {
+			"uid" : {"type":"string"},
+			"mail" : {"type":"string"},
+			"sn" : {"type":"string"},
+			"cn" : {"type":"string"},
+			"dn" : {"type":"string"},
+			"objectclass" : {"type":"array", "items":{"type": "string"}}
+		};
+		var report = env.validate(results[0], schema);
+		equal(report.errors.length, 0, JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
+asyncTest( "getAllUsers", 2, function() {
+	$.ajax({
+		url: urls.getAllUsers,
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		ok(results.length > 0, "Retorna datos");
+		var schema = {
+			"uid" : {"type":"string"},
+			"mail" : {"type":"string"},
+			"sn" : {"type":"string"},
+			"cn" : {"type":"string"},
+			"dn" : {"type":"string"},
+			"objectclass" : {"type":"array", "items":{"type": "string"}}
+		};
+		var report = env.validate(results[0], schema);
+		equal(report.errors.length, 0, JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
+asyncTest( "getAllUserNames", 2, function() {
+	$.ajax({
+		url: urls.getAllUserNames,
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		ok(results.length > 0, "Retorna datos");
+		var schema = {"type":"array", "items":{"type": "string"}};
+		var report = env.validate(results, schema);
+		equal(report.errors.length, 0, JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
+
+asyncTest( "deleteUser", 1, function() {
+	$.ajax({
+		url: urls.deleteUser,
+		data: {uid: 'vpascual'},
+		dataType: 'jsonp'
+	}).done(function(results){
+		console.debug(results);
+		deepEqual(results, {OK: "User delete"}, "Passed:"+ JSON.stringify(results));
+		start();
+	}).fail(function(results){
+		console.debug(results);
+		ok( false, "Fail and ready to resume!" );
+		start();
+	});	
+});
+
 asyncTest( "llistarUsuaris", 1, function() {
 	$.ajax({
 		url: urls.llistarUsuaris,
@@ -57,40 +266,3 @@ asyncTest( "llistarUsuarisJson", 2, function() {
 		start();
 	});	
 });
-
-asyncTest( "updatePassword ok", 1, function() {
-	$.ajax({
-		url: urls.updatePassword,
-		data: {codiIdec: 'admin', clau:'idec2001'},
-		method: 'post',
-		dataType: 'jsonp',
-	}).done(function(results){
-		console.debug(results);
-		deepEqual(results, {OK: "Password updated."}, "Passed:"+ results);
-		start();
-	}).fail(function(results){
-		console.debug(results);
-		ok( false, "Fail!:" + results);
-		start();
-	});	
-});
-
-asyncTest( "updatePassword no arguments", 1, function() {
-	$.ajax({
-		url:  urls.updatePassword,
-		data: {codiIdec: 'admin'},
-		method: 'post',
-		dataType: 'jsonp',
-	}).done(function(results){
-		console.debug(results);
-		deepEqual(results, {ERROR: "Arguments no vàlids."}, "Passed:" + results);
-		start();
-	}).fail(function(results){
-		console.debug(results);
-		ok( false, "Fail!:" + results );
-		start();
-	});
-	
-});
-
-
