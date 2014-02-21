cea8af69a246546b2469744cf91f5ca632e17573
bolollo <bolosig@gmail.com>
2014-02-21 13:47:36 +0100

    ip fuera

--- geocatweb/js/geocat.config-1.0.0.js
+++ geocatweb/js/geocat.config-1.0.0.js
@@ -25,10 +25,11 @@ var from_creaCapa="creaCapa";
 
 var num_max_pintxos = 250;  
 
-var HOST_APP = "http://84.88.72.36/";
-var GEOCAT02 = "http://84.88.72.36";
+//var HOST_APP = "http://84.88.72.36/";
+//var GEOCAT02 = "http://84.88.72.36";
+var GEOCAT02 = "http://172.70.1.12";
 //var HOST_APP = "http://geocat02.icc.local:8080/";
-//var HOST_APP = "http://localhost:8080/";
+var HOST_APP = "http://localhost:8080/";
 var paramUrl = {
 	proxy:"/maps/proxy.cgi",
 	uploadproxy:"/maps/upload.cgi",
