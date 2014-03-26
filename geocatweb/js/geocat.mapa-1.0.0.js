1612c150e3170a72040e61b9485c137185aaea8c
Jessica <jessisena@gmail.com>
2014-03-26 10:44:29 +0100

    2on commit

--- geocatweb/js/geocat.mapa-1.0.0.js
+++ geocatweb/js/geocat.mapa-1.0.0.js
@@ -162,16 +162,13 @@ function loadApp(){
 								 	nom: newValue, 
 								 	uid: $.cookie('uid')
 								}
-					
 								updateMapName(data).then(function(results){
 									_gaq.push(['_trackEvent', 'mapa', 'editar nom aplicacio', 'label editar nom', tipus_user]);
 									if(results.status=='OK') $('#dialgo_publicar #nomAplicacio').val(results.results);
-									console.debug($('#dialgo_publicar #nomAplicacio').val());
 								},function(results){
-									$('#nomAplicacio').html(mapConfig.nomAplicacio);				
+									$('#nomAplicacio').val(mapConfig.nomAplicacio);				
 								});	
 							}
-				
 						});						
 						
 					});
