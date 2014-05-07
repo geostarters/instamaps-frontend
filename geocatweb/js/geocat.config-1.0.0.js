f5d5a0d8abfbb20193585bcd56206ffb2447eb29
bolollo <bolosig@gmail.com>
2014-05-07 14:43:29 +0200

    comentar el scroll starwars

--- geocatweb/js/geocat.config-1.0.0.js
+++ geocatweb/js/geocat.config-1.0.0.js
@@ -41,8 +41,8 @@ var proxydir = "maps";
 
 var urlApp=document.location.href;
 if((urlApp.indexOf('localhost')!=-1)||(urlApp.indexOf('.local')!=-1)){
-	HOST_APP = "http://172.70.1.12/";
-//	HOST_APP = "http://localhost:8080/";
+	//HOST_APP = "http://172.70.1.12/";
+	HOST_APP = "http://localhost:8080/";
 	GEOCAT02 = "http://172.70.1.12";
 	proxydir="maps"; //he creat un director maps al meu Apache
 }
