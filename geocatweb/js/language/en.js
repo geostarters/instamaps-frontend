91f1ee80464519e09255e75a4323ec82322decec
bolollo <bolosig@gmail.com>
2015-11-27 14:14:39 +0100

    fix faltan : en archivos de idiomas

--- geocatweb/js/language/en.js
+++ geocatweb/js/language/en.js
@@ -103,7 +103,7 @@ jquery_lang_js.prototype.lang.en = {
 ,"Categories creades":"Categories created"
 ,"Centrar mapa a la seva ubicació":"Center map on your location"
 ,"Centre geomètric":"Centroid"
-,"Cercar catàleg IDEC","Search into IDEC catalog"
+,"Cercar catàleg IDEC":"Search into IDEC catalog"
 ,"Cercar llocs o coordenades ...":"Find places or coordinates ..."
 ,"Cercador de Topònims":"Place names search"
 ,"Cercador mundial de Topònims":"Search World Place Names"
