259e662f0b8d8cfc4353c223df2b0e1dec951c91
m.ortega <m.ortega@icgc.cat>
2015-12-03 14:25:16 +0100

    Fix merge HEAD

--- geocatweb/js/geocat.panel-capes.js
+++ geocatweb/js/geocat.panel-capes.js
@@ -498,7 +498,6 @@ function addFuncioRemoveLayer(){
 		var data = $this.data("data");
 		var obj = $this.data("obj");
 		
-<<<<<<< HEAD
 		//console.info("data");
 		//console.info(data);
 		
@@ -507,45 +506,6 @@ function addFuncioRemoveLayer(){
 		
 		 removeAtomicLayer(data,obj);
 					
-=======
-		removeServerToMap(data).then(function(results){
-			if(results.status==='OK'){
-				emptyMapLegendEdicio(obj.layer);
-				map.closePopup();
-				map.removeLayer(obj.layer);
-				//Eliminem la capa de controlCapes
-				controlCapes.removeLayer(obj);
-				
-				//actualitzem valors zindex de la resta si no es sublayer
-				if(!obj.sublayer){
-					var removeZIndex = obj.layer.options.zIndex;
-					controlCapes._lastZIndex--;
-					var aux = controlCapes._layers;
-					for (var i in aux) {
-						if (aux[i].layer.options.zIndex > removeZIndex) aux[i].layer.options.zIndex--;
-					}
-					//Eliminem les seves sublayers en cas que tingui
-					for(indexSublayer in obj._layers){
-						map.removeLayer(map._layers[indexSublayer]);
-					}
-				}
-
-				//Actualitzem capaUsrActiva
-				if(capaUsrActiva!=null && capaUsrActiva.options.businessId == obj.layer.options.businessId){
-					capaUsrActiva.removeEventListener('layeradd');
-					capaUsrActiva = null;
-				}				
-				
-				deleteServerRemoved(data).then(function(results){
-					//se borran del listado de servidores
-				});
-			}else{
-				return;//SI no ha anat be el canvi a BD. que no es faci tampoc a client, i es mostri un error
-			}				
-		},function(results){
-			return;//SI no ha anat be el canvi a BD. que no es faci tampoc a client, i es mostri un error
-		});					
->>>>>>> refs/heads/master_geolocal_montse
 	});	
 	
 	
