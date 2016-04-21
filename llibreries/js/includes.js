d6fd439fac2ae171b15d509dcb86068d334e1308
v.pascual <victor.pascual@icc.cat>
2016-04-21 16:28:13 +0200

    No simplificar punts

--- llibreries/js/includes.js
+++ llibreries/js/includes.js
@@ -88,17 +88,17 @@ L.LayerGroup.include({
          }         
          
          
-         
-         try{
-        	 _newJson = turf.simplify(json, 0.00001, false);
-
-        	 json.geometry=_newJson.geometry;
-        	 
-        	}catch(err){
-        		console.debug(err);
-
-        	}
-         
+         if(json.geometry.type != 'Point'){
+	         try{
+	          _newJson = turf.simplify(json, 0.00001, false);
+	
+	          json.geometry=_newJson.geometry;
+	          
+	         }catch(err){
+	         	console.debug(err);
+	
+	         }
+     }
          
          
          jsons.push(isGeometryCollection ? json.geometry : L.GeoJSON.asFeature(json));
@@ -177,17 +177,17 @@ L.LayerGroup.include({
          }
 
          
-         
-         try{
-          _newJson = turf.simplify(json, 0.000001, false);
-
-          json.geometry=_newJson.geometry;
-          
-         }catch(err){
-         	console.debug(err);
-
+         if(json.geometry.type != 'Point'){
+		         try{
+		          _newJson = turf.simplify(json, 0.000001, false);
+		
+		          json.geometry=_newJson.geometry;
+		          
+		         }catch(err){
+		         	console.debug(err);
+		
+		         }
          }
-         
          
          
          jsons.push(isGeometryCollection ? json.geometry : L.GeoJSON.asFeature(json));
