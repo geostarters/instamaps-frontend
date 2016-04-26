b2826a858e6ec5098ce0aef4f90bd9893a4b2672
m.ortega <m.ortega@icgc.cat>
2016-04-26 10:09:48 +0200

    Issues etiquetes

--- geocatweb/js/leaflet/L.IM_Label.js
+++ geocatweb/js/leaflet/L.IM_Label.js
@@ -47,6 +47,15 @@
 	      this._showLabel({
 	        latlng: pointM
 	      });
-	    }
+	    },
+	    unbindLabel: function () {
+	    	console.debug(this);
+			if (this.label) {
+				this._hideLabel();
+				this.label = null;
+				this._showLabelAdded = false;				
+			}
+			return this;
+		}
 	  });
 }(window, document));
