b4c102e4ac869f3cf414ad46bab941846523f00c
v.pascual <victor.pascual@icc.cat>
2016-09-20 13:06:54 +0200

    Versio Cesium 3D tiles

--- geocatweb/js/instamaps.mapa.3D-1.0.0.js
+++ geocatweb/js/instamaps.mapa.3D-1.0.0.js
@@ -24,7 +24,7 @@ var testModel3D=false;
 var msgHTML = "";
 //var _urlTerrenys = '/terrenys/demextes'; //'/cesium/terrenys/demextes'
 var _urlTerrenys = 'http://tilemaps.icgc.cat/terrenys/demextes'; //'/cesium/terrenys/demextes'
-var _urlModels3D='/terrenys/model3D/test/Prova1_cesium.json';
+var _urlModels3D='/cesium/terrenys/model3D/test/Prova1_cesium.json';
 
 var appl='mapa';
 var factorNavegador=1000;
@@ -1276,8 +1276,8 @@ var that = this;
 						var dataL = dataSource;
 						var XYZ_Edificis = [];							
 						setTimeout(function(){
-						that.calculaMatriuAlcades(dataL, XYZ_Edificis, 3, visible, msg);
-						//that.calculaMatriuAlcadesClaimTerrain(dataL, XYZ_Edificis, 3, visible, msg);
+						//that.calculaMatriuAlcades(dataL, XYZ_Edificis, 3, visible, msg);
+						that.calculaMatriuAlcadesClaimTerrain(dataL, XYZ_Edificis, 3, visible, msg);
 						},factorNavegador);												
 					}).otherwise(function (error) {
 						console.warn(error);
@@ -2003,7 +2003,7 @@ var that = this;
 
 					var pinBuilder = new PinBuilder_IM();
 					entity.billboard.color = Cesium.Color.WHITE;
-
+					entity.billboard.heightReference=Cesium.HeightReference.NONE ;
 					if (entity.properties.styles.icon.options.markerColor) {
 						
 						var colorPUNT = entity.properties.styles.icon.options.markerColor; //
