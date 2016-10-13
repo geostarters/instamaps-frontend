41dc74310265a539b158b4eb8e6f4bf19e39f34d
ICGC\i.besora <isaac.besora@icgc.cat>
2016-10-13 10:46:52 +0200

    Modificació títol del mapa
    Es redueix el nom del mapa a 58 caràcters si el mapa no és embed o bé a
    47 si ho és. D'aquesta forma, encara que l'usuari no estigui logat o el
    mapa sigui un embed, la barra queda fixa.

--- geocatweb/js/instamaps.visor.visor-2.0.0.js
+++ geocatweb/js/instamaps.visor.visor-2.0.0.js
@@ -993,11 +993,13 @@
 
 		_mapNameShortener: function(inName) {
 
+			var self = this;
 			var name = inName
-			if(60 < name.length)
+			var maxLength = (self.embed ? 47 : 58);	//Limitem el nombre de caràcters del títol pq no salti de línia
+			if(maxLength < name.length)
 			{
 
-				name = "<span title=\"" + inName + "\">" + name.substring(0, 60) + "...</span>";
+				name = "<span title=\"" + inName + "\">" + name.substring(0, maxLength-3) + "...</span>";
 
 			}
 
