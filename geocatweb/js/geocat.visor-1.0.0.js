68159c80d83866bd11bcf47afc495a1aac6eb523
m.ortega <m.ortega@icgc.cat>
2016-11-15 14:59:42 +0100

    Issues pujada

--- geocatweb/js/geocat.visor-1.0.0.js
+++ geocatweb/js/geocat.visor-1.0.0.js
@@ -290,7 +290,9 @@ function loadApp(){
 function loadPublicMap(results){
 	mapConfig = $.parseJSON(results.results);
 
-	$('meta[property="og:title"]').attr('content', "Mapa "+mapConfig.nomAplicacio);
+	if ($(location).attr('href').indexOf('/visor.html') != -1) { 
+		$('meta[property="og:title"]').attr('content', "Mapa "+mapConfig.nomAplicacio);
+	}
 
 	var nomUser = mapConfig.entitatUid.split("@");
 	var nomEntitat = mapConfig.nomEntitat;
@@ -321,12 +323,12 @@ function loadPublicMap(results){
 		desc==""?desc=mapConfig.nomAplicacio:desc=desc;
 		
 		if (desc!=undefined) desc=desc.replaceAll("'","\'");
-
-		$('meta[name="description"]').attr('content', desc+' - Fet amb InstaMaps.cat');
-		$('meta[property="og:description"]').attr('content', desc+' - Fet amb InstaMaps.cat');
-
-		var urlThumbnail = GEOCAT02 + paramUrl.urlgetMapImage+ "&request=getGaleria&update=false&businessid=" + url('?businessid');
-		$('meta[property="og:image"]').attr('content', urlThumbnail);
+		if ($(location).attr('href').indexOf('/visor.html') != -1) {
+			$('meta[name="description"]').attr('content', desc+' - Fet amb InstaMaps.cat');
+			$('meta[property="og:description"]').attr('content', desc+' - Fet amb InstaMaps.cat');	
+			var urlThumbnail = GEOCAT02 + paramUrl.urlgetMapImage+ "&request=getGaleria&update=false&businessid=" + url('?businessid');
+			$('meta[property="og:image"]').attr('content', urlThumbnail);
+		}
 
 		if (mapConfig.options.description!=undefined) infoHtml += '<p>'+mapConfig.options.description+'</p>';
 		if (mapConfig.options.tags!=undefined) infoHtml += '<p>'+mapConfig.options.tags+'</p>';
