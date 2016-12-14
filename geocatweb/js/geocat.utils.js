20da7217e444f188eb839c7df6ecddf49b42f717
v.pascual <victor.pascual@icc.cat>
2016-12-14 08:49:55 +0100

    utils nous

--- geocatweb/js/geocat.utils.js
+++ geocatweb/js/geocat.utils.js
@@ -14,7 +14,7 @@ function isValidEmailAddress(emailAddress) {
 }
 
 function isValidURL(url) {
-	var pattern = /((http(s)?|ftp):\/\/.)?(www\.)?[-a-zA-Z0-9:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9%_:?\+.~#&//=]*)/;
+	var pattern = /((http(s)?|ftp):\/\/.)?(www\.)?[-a-zA-Z0-9:%._\+~#=]{2,256}\.[a-z0-9]{2,6}\b([-a-zA-Z0-9%_:?\+.~#&//=]*)/;
 	return pattern.test(url);
 }
 
