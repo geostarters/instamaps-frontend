be9bdd7efec56ffadd75cf88257870a88a61bdf6
m.ortega <m.ortega@icgc.cat>
2017-04-11 10:14:46 +0200

    Pujar JSON a Instamaps

--- geocatweb/js/geocat.mapa.url-file.js
+++ geocatweb/js/geocat.mapa.url-file.js
@@ -93,8 +93,9 @@ function createURLfileLayer(urlFile, tipusFile, epsgIN, dinamic, nomCapa, colX, 
 		jQuery('#info_uploadFile').show();
 
 
-	   if (param_url.indexOf("/opt/")>-1){
-			urlFile=HOST_APP+"/jsonfiles/"+param_url.substring(param_url.lastIndexOf("/")+1,param_url.length);
+	   if (param_url.indexOf("/opt/")>-1 || param_url.indexOf("\\temp\\")>-1 ){
+		    if (param_url.indexOf("\\temp\\")>-1)  urlFile=HOST_APP+"/jsonfiles/"+param_url.substring(param_url.lastIndexOf("\\")+1,param_url.length);
+		    else  urlFile=HOST_APP+"/jsonfiles/"+param_url.substring(param_url.lastIndexOf("/")+1,param_url.length);
 			param_url = paramUrl.urlFileDin	+"tipusFile=" + ".geojson"+
 			"&tipusAcc="+tipusAcc+
 			"&tipusCodi="+tipusCodi+
