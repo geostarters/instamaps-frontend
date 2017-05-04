ef8e3e930c7a3985b01d593f74ddd97c9e46b0c5
m.ortega <m.ortega@icgc.cat>
2017-05-04 09:07:19 +0200

    Càrrega fitxers JSON

--- geocatweb/js/geocat.config-1.0.0.js
+++ geocatweb/js/geocat.config-1.0.0.js
@@ -16,6 +16,7 @@ if((urlApp.indexOf('localhost')!=-1)||(urlApp.indexOf('.local')!=-1)){
 //	HOST_APP2 = "http://nicosia.icgc.local/";
 	HOST_APP = "http://localhost/";//Local Jess
 	HOST_APP2 = "http://localhost/";
+	HOST_APP3 = "http://localhost/";
 
 //	HOST_APP = "http://localhost/";//Local Jess
 //	GEOCAT02 = "http://localhost:8181";
@@ -32,6 +33,7 @@ var DOMINI = "www.instamaps.cat";
 if(urlApp.indexOf('172.70.1.11')!=-1){
 	HOST_APP = "http://172.70.1.11/";
 	HOST_APP2 = "http://172.70.1.11/";
+	HOST_APP3 = "http://172.70.1.11/";
 //	HOST_APP = "http://localhost:8080/";
 	GEOCAT02 = "http://172.70.1.11";
 	HOST_GEOLOCAL = "http://geolocaldev.icgc.local/";
