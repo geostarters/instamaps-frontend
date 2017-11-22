fb21193a49549dbbe68e5c5da68c600a97f47a53
Ammont82 <ammont82@gmail.com>
2017-11-22 15:36:33 +0100

    Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,7 @@ su nginx
 git fetch origin production
 git fetch --tags
 rm geocatweb/dist/template_visor.html
-rm geocatweb/templates/template_visor.html
-rm geocatweb/templates/visor.html
-rm geocatweb/visor.html
-rm geocatweb/mapa.html
-rm geocatweb/js/instamaps.version.js
-rm geocatweb/js/instamaps.version.json
+git reset --hard
 git pull origin production
 chown -R nginx:nginx *
 chmod 777 -Rf maps
